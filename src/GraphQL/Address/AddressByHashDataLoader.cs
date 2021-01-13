2e3fc7df6ca969d3afa4d834b0f6cc653f88c5e9
Clark Alesna <clark_alesna@hotmail.com>
2021-01-13 15:23:17 +0800

    Docs Folder and Adjustments

--- src/GraphQL/Address/AddressByHashDataLoader.cs
+++ src/GraphQL/Address/AddressByHashDataLoader.cs
@@ -30,23 +30,12 @@ namespace GCQL.NET.Server.GraphQL
         {
             await using CardanoDbContext dbContext =
                 _dbContextFactory.CreateDbContext();
-            
-            // var txOuts = await dbContext.TxOuts
-            //     .Where(o => keys.Contains(o.Address))
-            //     .GroupJoin(
-            //         dbContext.TxIns, o => new {o.TxId, TxIndex = o.Index},
-            //         i => new {TxId = i.TxOutId, TxIndex = i.TxOutIndex}, 
-            //         (o, i) => new {o.Address, o.Value, In = i})
-            //     .DefaultIfEmpty()
-            //     .ToListAsync(cancellationToken);
 
             var txOuts = await (from o in dbContext.TxOuts
                 where keys.Contains(o.Address)
                 join i in dbContext.TxIns on new {o.TxId, TxIndex = o.Index} equals new {TxId = i.TxOutId, TxIndex = i.TxOutIndex}  into gj
                 from tx in gj.DefaultIfEmpty()
                 select new { o.Address, o.Value, TxInId = tx != null ? tx.TxInId : 0L, o.TxId }).ToListAsync(cancellationToken);
-            // Query To Sum All Value
-            // Query To Count Transactions
 
             return txOuts.GroupBy(o => o.Address).Select(o => 
                     new Address(
